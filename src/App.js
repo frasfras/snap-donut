1a9ac0ed20b98c35668f13a88fb650c52cdb4b66
frasfras <65541080+frasfras@users.noreply.github.com>
2025-02-05 01:18:06 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import BaseballMetricsAnalyzer from './BaseballMetricsAnalyzer';
+import BaseballMetricsAnalyzer from './BaseballMetricsAnalyzerx';
 
 function App() {
   return (
